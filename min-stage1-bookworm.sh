052999fc84363b064daa51411a7da01467e0234a
Jason Hsu <jhsu802701@jasonhsu.com>
2023-10-28 17:59:07 -0500

    Successfully copies and updates set-variables script

--- min-stage1-bookworm.sh
+++ min-stage1-bookworm.sh
@@ -9,3 +9,23 @@ rm -rf tmp
 
 mkdir -p tmp/bin
 cp bin/set-variables tmp/bin
+
+ABBREV='min-stage1'
+SUITE='bookworm'
+OWNER='rubyonracetracks'
+DISTRO='debian'
+DIR_ROOT=`pwd`
+
+update-bin-set-variables () {
+  STRING1="$1"
+  STRING2="$2"
+  wget -O - \
+    https://raw.githubusercontent.com/rubyonracetracks/docker-common/main/replace-string-in-file.sh \
+    | bash -s "$STRING1" "$STRING2" 'tmp/bin/set-variables'
+}
+
+update-bin-set-variables '<ABBREV>' "$ABBREV"
+update-bin-set-variables '<SUITE>' "$SUITE"
+update-bin-set-variables '<OWNER>' "$OWNER"
+update-bin-set-variables '<DISTRO>' "$DISTRO"
+update-bin-set-variables '<DIR_ROOT>' "$DIR_ROOT"
